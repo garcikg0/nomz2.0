2ea78f4c5b5a63ef6219029abf34584001a95d58
garcikg0 <kg.gar91@gmail.com>
2020-11-13 23:22:30 -0500

    rails migration reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_152947) do
+ActiveRecord::Schema.define(version: 2020_11_14_040648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "icons", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "image_link"
+    t.string "ingredients"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.bigint "kitchen_id", null: false
+    t.string "name"
+    t.string "storage"
+    t.string "icon"
+    t.string "status"
+    t.string "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["kitchen_id"], name: "index_ingredients_on_kitchen_id"
+  end
+
+  create_table "kitchens", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_kitchens_on_user_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.bigint "kitchen_id", null: false
+    t.string "name"
+    t.string "image"
+    t.string "source"
+    t.string "url"
+    t.string "ingredient_lines"
+    t.string "ingredients"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["kitchen_id"], name: "index_recipes_on_kitchen_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -26,4 +71,7 @@ ActiveRecord::Schema.define(version: 2020_11_02_152947) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "ingredients", "kitchens"
+  add_foreign_key "kitchens", "users"
+  add_foreign_key "recipes", "kitchens"
 end
